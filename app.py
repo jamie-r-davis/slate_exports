33e24c8398410a851b172289455737a7bbe1766f
jamie-r-davis <jamjam@umich.edu>
2019-03-25 12:49:04 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import sys
+
+from loguru import logger
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils.login import login
+
+import config
+
+
+class ContextDriver(webdriver.Chrome):
+    def __enter__(self):
+        return self
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.quit()
+
+logger.remove()
+logger.add(sys.stdout, level='INFO', format='<green>{time}</green> <level>{message}</level>')
+logger.add('app.log', level='INFO', format='<green>{time}</green> <level>{message}</level>')
+
+def main():
+    options = Options()
+    options.headless = True
+    # init headless webdriver
+    with ContextDriver(options=options) as d:
+        # authenticate with u-m
+        d.get('https://weblogin.umich.edu')
+        login(d, **config.AUTH)
+        # visit each env to establish session cookies
+        for env in config.SLATE_ENVS:
+            d.get(env['host']+'/manage')
+            logger.debug(d.current_url)
+            # init requests session
+            s = requests.session()
+            # grab cookies from webdriver session
+            for cookie in d.get_cookies():
+                s.cookies.set(cookie['name'], cookie['value'])
+            query_url = '{host}{endpoint}'
+            r = s.get(query_url.format(**env))
+            logger.debug(r.json())
+            for q in r.json()['row']:
+                export_url = '{host}/manage/service/export?id={id}'
+                r2 = s.get(export_url.format(host=env['host'], id=q['id']))
+                logger.info(f"[{env['host']}] {r2.status_code}: {r2.text} ({q['name']})")
+
+if __name__ == '__main__':
+    main()
---
+++ sample_config.py
@@ -0,0 +1,17 @@
+# username/password for account
+AUTH = {
+    'username': 'user',
+    'password': 'secret-pw'
+}
+
+# list of environment objects
+SLATE_ENVS = [
+    {
+        'host': 'https://umi.test.technolutions.net',
+        'endpoint': '/manage/query/run?id=e423b397-f25b-465d-a6fb-90bddd4a2068&h=2b5cb62c-fb10-14c3-281e-3d1725ac434b&cmd=service&output=json',
+    },
+    {
+        'host': 'https://umi-2.test.technolutions.net',
+        'endpoint':'/manage/query/run?id=7b2ae527-d043-4821-874c-a42f96d490bd&h=c344ffbf-c9ca-50f0-1f72-f1f14209d059&cmd=service&output=json',
+    },
+]
